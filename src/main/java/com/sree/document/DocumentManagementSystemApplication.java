19772f0a22d784a7ba04a421870ed5b48ac90c75
SreekanthGaddoju <93276331+SreekanthGaddojuMsx@users.noreply.github.com>
2024-04-09 13:48:58 +0200

    This commit to check the Jenkins configuration, is it picking the latest commit and builing my application or not?

--- src/main/java/com/sree/document/DocumentManagementSystemApplication.java
+++ src/main/java/com/sree/document/DocumentManagementSystemApplication.java
@@ -3,6 +3,9 @@ package com.sree.document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This Class is Main Class of Document Management System Application.
+ */
 @SpringBootApplication
 public class DocumentManagementSystemApplication {
 
